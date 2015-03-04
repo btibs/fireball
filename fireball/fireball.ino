4fe3277ec5d8a7a907d06abb80507fefe8f7444c
Beth McNany <beth.mcnany@gmail.com>
2015-03-04 16:03:51 -0500

    calibrated accel, added button and 7seg

--- fireball/fireball.ino
+++ fireball/fireball.ino
@@ -1,14 +1,17 @@
 #include <Wire.h>
 #include <Adafruit_LSM303.h>
+#include <SoftwareSerial.h>
 
 Adafruit_LSM303 lsm;
+SoftwareSerial Serial7Segment(7, 8); // RX, TX
+char tempString[10];
 
 // pin assignments
 const int accelX = 0;
 const int accelY = 1;
 const int accelZ = 2;
 
-const int buttonPin = 3;
+const int buttonPin = 15;
 const int maxDice = 7;
 
 // minimum change in detected angle to trigger an event
@@ -19,19 +22,23 @@ float pitch, roll, yaw, lastPitch, lastRoll, lastYaw;
 boolean isRolling;
 int nDice;
 
-// button press = 1 2 3 etc dice and cycle thru
-// but only when it's not rolling
-
 void setup() {
   pitch = 999;
   roll = 999;
   yaw = 999;
   isRolling = false;
-  
   nDice = 1;
-  pinMode(buttonPin, INPUT);
-  Serial.begin(9600);
   randomSeed(analogRead(0)); // should be a floating pin - change as necessary
+  
+  pinMode(buttonPin, INPUT);
+
+  Serial.begin(9600);
+  Serial7Segment.begin(9600);
+  Serial7Segment.write('v'); // reset display
+  Serial7Segment.write(0x7A); // brightness control
+  Serial7Segment.write((byte)1); // 0 bright 255 dim
+  sprintf(tempString, "%04d", 1234);
+  Serial7Segment.print(tempString);
   
   if (!lsm.begin()) {
     Serial.println("Unable to initialize LSM303!");
@@ -52,13 +59,12 @@ void loop() {
   float zAcc = lsm.accelData.z;
   
   // orientation math
-  // (for reference using glove accel, need to recalibrate)
-  // zeroes should be the minimum accel value
-  // ie, x and y when board is lying flat, z when straight up
-  float xZero = 508; // range like 450-600??
-  float yZero = 557;
-  float zZero = 519;
-  float scale = 556; // z value at rest i.e. gravity
+  // To calibrate: x_0 and y_0 when board is lying flat, z_0 when board vertical
+  // scale = gravity, i.e. z value when board is flat
+  float xZero = 8; // 8-24
+  float yZero = 0; //0-16
+  float zZero = -44; //-44 - 24
+  float scale = -928; // 920-940
   
   // update values
   lastPitch = pitch;
@@ -69,30 +75,34 @@ void loop() {
   roll = (atan(yAcc/sqrt(pow(xAcc, 2) + pow(zAcc, 2))) * (180/PI));
   yaw = (acos(zAcc/sqrt(pow(xAcc, 2) + pow(yAcc, 2) + pow(zAcc, 2))) * (180/PI)); 
   
-  // we just need to check if the ball has stopped rolling
-  // i.e., the accelerations have stabilized
-  // we can do this by keeping track of the deltas between readings
-  //Serial.println();
-  //Serial.println(abs(pitch-lastPitch));
-  //Serial.println(abs(roll-lastRoll));
-  //Serial.println(abs(yaw-lastYaw));
+  // check if the ball has stopped rolling
   if (abs(pitch-lastPitch) > deltaThreshold || abs(roll-lastRoll) > deltaThreshold || abs(yaw-lastYaw) > deltaThreshold) {
     // we have a roll, generate a new random number
-    isRolling = true;
     int n = random(1,(6*nDice)+1);
     Serial.print("ROLLING: ");
+    Serial.print(abs(pitch-lastPitch));
+    Serial.print(" ");
+    Serial.print(abs(roll-lastRoll));
+    Serial.print(" ");
+    Serial.print(abs(yaw-lastYaw));
+    Serial.print(" ");
     Serial.println(n);
     
-    // todo: display code here
-    // todo:? delay inversely proportional to roll speed
+    Serial7Segment.write('v'); // reset display
+    sprintf(tempString, " %2d ", n);
+    Serial7Segment.print(tempString);
   } else {
-    isRolling = false; // could use this to re-initialize a seq?
-    if (bPress == HIGH) {
+    if (bPress == LOW) {
       // only count button presses when ball is still
       nDice++;
       if (nDice > maxDice)
         nDice = 1;
+      sprintf(tempString, "d  %1d", nDice);
     }
+    Serial7Segment.write('v'); // reset display
+    Serial7Segment.print(tempString);
+    delay(100);
   }
+  delay(100);
 }
 
